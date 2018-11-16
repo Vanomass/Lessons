0d46eb403886d78a3901f7bf3361e3deb0ee7622
Vanya <ivanbatya3@gmail.com>
2018-11-16 19:04:28 +0300

    First commit

---
+++ JS.js
@@ -0,0 +1,15 @@
+let money = prompt('Ваш бюджет на месяц?', "20000");
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let appData = {
+  budget :  money,
+  timeData : time,
+  expenses : {answer1 : answer2 , answer3 : answer4},
+  optionalExpenses : {},
+  income : {},
+  savings : false
+}
+var answer1 = prompt('Введите обязательную статью расходов в этом месяце');
+var answer2 = prompt('Во сколько обойдется?');
+var answer3 = prompt('Введите обязательную статью расходов в этом месяцу');
+var answer4 = prompt('Во сколько обойдется?');
+alert('Бюджет на 1 день '+ (appData.budget - answer2 - answer4)/30);
