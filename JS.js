dec4bd2be9af4dd1fc1c8abefd87bf28d625920d
Vanya <ivanbatya3@gmail.com>
2018-11-18 16:46:30 +0300

    Third commit

--- JS.js
+++ JS.js
@@ -1,16 +1,43 @@
-let money = prompt('Ваш бюджет на месяц?', "20000");
+let money = +prompt('Ваш бюджет на месяц?', "20000");
 let time = prompt('Введите дату в формате YYYY-MM-DD');
-let answer1 = prompt('Введите обязательную статью расходов в этом месяце');
-let answer2 = prompt('Во сколько обойдется?');
-let answer3 = prompt('Введите обязательную статью расходов в этом месяцу');
-let answer4 = prompt('Во сколько обойдется?');
+
 let appData = {
   budget :  money,
   timeData : time,
-  expenses : {answer1 : answer2 , answer3 : answer4},
+  expenses : {},
   optionalExpenses : {},
   income : {},
   savings : false
 }
+/*
+for (let i = 0; i<2; i++){
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt('Во сколько обойдется?',"");
+  if( (typeof(a))==='string'&& (typeof(a)) !=null &&(typeof (b)) !=null
+  && a!='' && b != '' && a.length < 50 ) {
+    console.log("done");
+    appData.expenses[a] = b;}
+    else{
+      i=0;
+    }
+  }
+  */
+//alert('Бюджет на 1 день ' + (appData.budget - expenses[a]) / 30);
 
-alert('Бюджeт на 1 день '+ (appData.budget - answer2 - answer4)/30);
+/*
+let i = 0;
+do {
+let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+let b = prompt('Во сколько обойдется?', "");
+i++;
+}
+while(i<2);
+*/
+/*
+let i =0;
+while(i<2){
+let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+let b = prompt('Во сколько обойдется?', "");
+i++;
+}
+*/
